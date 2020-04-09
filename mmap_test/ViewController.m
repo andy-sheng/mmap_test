7bf6a9c0c935869f97f57d31a4c24e5815975de9
andysheng <andysheng@tencent.com>
2020-04-10 00:50:37 +0800

    add: mmap test

--- mmap_test/ViewController.m
+++ mmap_test/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <sys/mman.h>
 
 @interface ViewController ()
 
@@ -16,7 +17,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    off_t size = 100 * 1024 * 1024;
+    
+    NSString *mmapFile = [NSHomeDirectory() stringByAppendingPathComponent:@"voice.pcm"];
+    
+    
+    
+    int fp = open(mmapFile.UTF8String, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (ftruncate(fp, size)) {
+        NSLog(@"truncate error");
+    }
+    
+    short *ptr= mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fp, 0);
+    for (int i = 0; i < size / 2; ++i) {
+        ptr[i] = i;
+    }
+    
+    int cnt = 0;
+    for (int i = 0; i < size / 2; ++i) {
+        cnt += ptr[i];
+    }
+    NSLog(@"%ld", cnt);
 }
 
 
